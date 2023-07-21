849fc835bec2f38ce309fdcc4f2989e53a48ee3f
Christian Lütke Stetzkamp <christian@lkamp.de>
2023-07-21 16:35:41 +0200

    spring: Add exmpl spring-boot app

---
+++ spring/src/test/java/com/github/clkamp/promexmpspring/PromExmpSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.clkamp.promexmpspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromExmpSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
